338b3b56d91115ddba6696ead3ddf92f3993a502
ben <ben.lemoine.bl@gmail.com>
2016-11-13 10:48:13 +0100

    Fixe les articles "bandeau"

--- quick-news.php
+++ quick-news.php
@@ -1,9 +1,5 @@
-<?php if(has_posts()):
-while(posts()): if(article_category_slug() == "quick-news"): ?>
-
+<?php   while(latest_posts()): if(article_category_slug()=="quick-news"): ?>
     <article class="quick-news <?php echo article_custom_field('theme'); ?>">
         <p><?php echo article_markdown(); ?></p>
     </article>
-
-<?php endif; endwhile;
-endif; ?>
+<?php   endif; endwhile; ?>
