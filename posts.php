aabf17436cee211726c2fbe34d61f4f3fe460851
ben <ben.lemoine.bl@gmail.com>
2016-11-13 10:48:23 +0100

    Modifie l'affichage de la liste des articles

--- posts.php
+++ posts.php
@@ -1,36 +1,41 @@
 <?php   theme_include('header');
         theme_include('quick-news'); ?>
-<div class="wrap-main">
-<?php   theme_include('slider'); ?>
+<div class="wrap-main posts">
+    <section class="main">
+        <section class="posts full">
+            <?php if(has_posts()): ?>
+            <ul>
+                <?php while(posts()): if(article_category_slug()<>"quick-news"): ?>
+                    <li class='post'>
+                        <header>› <a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><h3 class="title"><?php echo article_title(); ?></h3></a>
 
-<section class="main">
-    <section class="posts">
-        <h1 class="news">Les derniers <b>articles</b> à lire</h1>
-        <?php if(has_posts()): ?>
-        <ul>
-            <?php while(posts()): if(article_category_slug() != "quick-news"): ?>
-                <li class='post'>
-                    › <a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><h3 class="title"><?php echo article_title(); ?></h3></a>
-                    
-                    <span class="infos"><span class="cat"><?php echo article_custom_field('theme'); ?></span> - <?php echo gmdate("d/m", article_time()); ?></span>
-                    
-                        
-                    <div class="excerpt">
-                        <a href="<?php echo article_url(); ?>" title="Lire l'article"><?php echo strip_tags(excerpt(article_html(), 205)); ?></a>
+                        <span class="infos"><span class="cat"><?php echo article_custom_field('theme'); ?></span> - <?php echo gmdate("d/m", article_time()); ?></span></header>
+
+
+                        <div class="excerpt">
+                            <a href="<?php echo article_url(); ?>" title="Lire l'article"><?php echo strip_tags(excerpt(article_html(), 405)); ?></a>
+                        </div>
+                    </li>
+                <?php endif; endwhile; ?>
+            </ul>
+            <?php if(has_pagination()): ?>
+                <nav class="pagination">
+                    <div class="previous">
+                        <?php echo posts_prev('Plus ancien'); ?>
+                    </div><!--
+                    --><div class="next">
+                        <?php echo posts_next('Plus récent'); ?>
                     </div>
-                </li>
-            <?php endif; endwhile; ?>
-        </ul>
-    </section><!--
-
-        <?php else : ?><!---->
+                </nav>
+            <?php endif;
+            else: ?>
             <ul>
                 <li class="post">
                     <b class='error'>Erreur.</b> Aucun résultat trouvé.
                 </li>
-            </ul><!--
-        <?php endif; ?>
+            </ul>
+            <?php endif; ?>
+        </section>
 
-<?php   theme_include('sidebar'); ?><!----->
 <a href="<?php echo base_url(); ?>admin/posts/" class="admin">Administration</a>
 <?php   theme_include('footer'); ?>
